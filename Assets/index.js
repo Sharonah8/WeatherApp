edd88af826d85e5fa65df756b524cc1d2fc304dc
Sharonah8 <nyamongosharon88@gmail.com>
2022-08-31 18:47:56 +0300

    Pick current date and time in realtime using getDate methods

--- Assets/index.js
+++ Assets/index.js
@@ -6,14 +6,23 @@ const countryDetails = document.getElementById('country');
 const weatherForecastDetails = document.getElementById('weather-forecast');
 const currentTemp = document.getElementById('current-temp');
 
-//setInterval function, a function that's called after specific intervals
+
+const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"];
+//setInterval method which calls a function after specific intervals(in milliseconds)
 setInterval(() => {
     const time = new Date();
     const month = time.getMonth();
     const date = time.getDate();
     const day = time.getDay();
     const hour = time.getHours();
+
+    //convert from 24-hour clock to 12-hour clock
     const hoursIn12HourFormat = hour >= 13 ? hour % 12 : hour
     const minutes = time.getMinutes();
-    const amPm = hours >= 12 ? 'PM' : 'AM'
+    const amPm = hour >= 12 ? 'PM' : 'AM'
+
+    timeDetails.innerHTML = hoursIn12HourFormat + ':' + minutes + ' ' + `<span id="am-pm">${amPm}</span>`
+
+    dateDetails.innerHTML = days[day] + ',' + date + ' ' + months[month]
 }, 1000);
