b0902c60472b2dd25665449e4181cd844180da93
Sharonah8 <nyamongosharon88@gmail.com>
2022-08-31 18:27:48 +0300

    Update date & time without the use of the weather API

--- Assets/index.js
+++ Assets/index.js
@@ -5,3 +5,15 @@ const timeZone = document.getElementById('time-zone');
 const countryDetails = document.getElementById('country');
 const weatherForecastDetails = document.getElementById('weather-forecast');
 const currentTemp = document.getElementById('current-temp');
+
+//setInterval function, a function that's called after specific intervals
+setInterval(() => {
+    const time = new Date();
+    const month = time.getMonth();
+    const date = time.getDate();
+    const day = time.getDay();
+    const hour = time.getHours();
+    const hoursIn12HourFormat = hour >= 13 ? hour % 12 : hour
+    const minutes = time.getMinutes();
+    const amPm = hours >= 12 ? 'PM' : 'AM'
+}, 1000);
