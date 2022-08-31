2fcb23d123d0b296e8442baba1c70c08d722a355
Sharonah8 <nyamongosharon88@gmail.com>
2022-08-31 19:18:22 +0300

    Get latitude and longitude coordinates

--- Assets/index.js
+++ Assets/index.js
@@ -9,6 +9,9 @@ const currentTemp = document.getElementById('current-temp');
 
 const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 const months = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+const API_KEY = '4e8a0605cdbd2cb0f94cb18402d9e393'
+
 //setInterval method which calls a function after specific intervals(in milliseconds)
 setInterval(() => {
     const time = new Date();
@@ -26,3 +29,13 @@ setInterval(() => {
 
     dateDetails.innerHTML = days[day] + ',' + date + ' ' + months[month]
 }, 1000);
+
+//Getting data from the API 
+getWeatherData();
+function getWeatherData() {
+    navigator.geolocation.getCurrentPosition((success) => {
+        console.log(success);
+
+        let { latitude, longitute } = success.coords;
+    })
+}
