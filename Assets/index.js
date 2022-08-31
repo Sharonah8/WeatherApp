87e82a5b768d105a9a1d3e99b1ed7a06dd25e9b1
Sharonah8 <nyamongosharon88@gmail.com>
2022-08-31 18:21:33 +0300

    Access elements on html in the js file

---
+++ Assets/index.js
@@ -0,0 +1,7 @@
+const timeEl = document.getElementById('time');
+const dateEl = document.getElementById('date');
+const currentWeatherItemsEl = document.getElementById('current-weather-items');
+const timeZone = document.getElementById('time-zone');
+const countryDetails = document.getElementById('country');
+const weatherForecastDetails = document.getElementById('weather-forecast');
+const currentTemp = document.getElementById('current-temp');
